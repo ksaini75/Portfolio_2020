1f67ecb747fa6f5bc2fafc966f058889d5b4282c
Karishma Saini <ksaini7595@gmail.com>
2020-02-11 20:26:12 -0500

    adding the build folder

---
+++ build/precache-manifest.860f5e60905b8f38ade3a7c14653072c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4db3aa8318019bc00c38c7cdb548ea82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "704d54d575e84dbf5337",
+    "url": "/static/css/2.16d1700a.chunk.css"
+  },
+  {
+    "revision": "6ce9aa327fbc419f8eb2",
+    "url": "/static/css/main.6f460b2f.chunk.css"
+  },
+  {
+    "revision": "704d54d575e84dbf5337",
+    "url": "/static/js/2.ca18b288.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.ca18b288.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6ce9aa327fbc419f8eb2",
+    "url": "/static/js/main.2b788def.chunk.js"
+  },
+  {
+    "revision": "2aeb167d877ecaea750b",
+    "url": "/static/js/runtime-main.e5531be7.js"
+  },
+  {
+    "revision": "17ce4d37daf210ba83cd3f4cae8bfe38",
+    "url": "/static/media/Karishma_Saini_Resume.17ce4d37.pdf"
+  },
+  {
+    "revision": "d3e8213bd2b08f327d2d3ec2b051d9f4",
+    "url": "/static/media/YelpCamp.d3e8213b.png"
+  },
+  {
+    "revision": "5757c9f291f22e508705e5cb31c4c4fa",
+    "url": "/static/media/faceRecognition.5757c9f2.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "30af2c963db690c9ddf41bae4c425559",
+    "url": "/static/media/roboFriends.30af2c96.png"
+  }
+]);
